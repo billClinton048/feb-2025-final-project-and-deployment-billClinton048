7d4c6f3a27f0a30302a06a70f477b8bdcc31b748
billClinton048 <clintonkaranja46@gmail.com>
2025-05-21 07:01:53 +0300

    message

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Toggle dark/light theme
+const toggleBtn = document.getElementById("toggle-theme");
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
